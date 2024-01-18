5c9b8182c00d3e6c99ec375da3eea44adbd8e236
soriyahor <hor.soriya@gmail.com>
2024-01-18 19:58:56 +0100

    ajout def score_grand_suite

--- fonctions_score.py
+++ fonctions_score.py
@@ -1,11 +1,20 @@
 
 
 def is_yams(dices: list):
-    x=dices[0][1]
+    x=dices[0]
     for r in dices:
-        print(f"x={x} r = {r[1]}")
-        if r[1] != x:
+        print(f"x={x} r = {r}")
+        if r != x:
             return 0
         else: 
-            x=r[1]
+            x=r
     return 50  
+
+def score_grand_suite(dices: list):
+    dices.sort()
+    print(f"gds{dices}")
+    for i in range(len(dices)-1):
+        print(f"i={dices[i]} i+1={dices[i+1]}  ")
+        if (dices[i]+1) != dices[i+1]:
+            return 0
+    return 40
